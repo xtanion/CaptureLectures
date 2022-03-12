b24c45cff99507a1b9fd56fa85eaa2a135850be2
Xtanion <anandshivam54321@gmail.com>
2022-03-12 23:39:59 +0530

    Using scikit to remove duplicates in different thread

--- web/app.py
+++ web/app.py
@@ -3,13 +3,10 @@ from flask import Flask, render_template, request, redirect, url_for
 import cv2
 from PIL import Image
 from skimage.metrics import structural_similarity
+import  threading
 
 ALLOWED_TYPE = {'mp4', 'mkv', 'webm', 'mov'}
 app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_video():
@@ -21,12 +18,55 @@ def upload_video():
             # TODO check for files type
             file.save(os.path.join(
                 'web/uploads', filename))
+
+            with app.app_context(), app.test_request_context():
+                threading.Thread(target=getImages).start()
+            # getImages()
+
             return render_template('output.html')
 
     return render_template('index.html')
 
-class ImageConverter():
-    
+# Using Scikit-Learn to get Image data
+
+def getImages():
+    print('Running on thread')
+    try:
+        path = os.path.join('web/uploads', 'demo0.mp4')
+        print("Path found")
+    except:
+        print('ERROR occured')
+    capture = cv2.VideoCapture(path)
+    i = 0
+    print("captured")
+    lag, frame = capture.read()
+    img0 = frame
+    img0_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while(capture.isOpened()):
+        img0 = frame
+        img0_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        flag, frame = capture.read()
+        if(flag == False):
+            break
+
+        img1_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        if (purge_duplicate(img0_bw, img1_bw, 0.65, i) == True):
+            path = 'web/frame/'+str(i)+'.jpg'
+            print(path)
+            cv2.imwrite(path, img0)
+            i += 1
+
+    capture.release()
+    return render_template('output.html')
+
+def purge_duplicate(img0, img1, threshold, i=0):
+    sim, diff = structural_similarity(img0, img1, full=True)
+    if(sim < threshold):
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ web/capture_frame.py
@@ -0,0 +1,41 @@
+import cv2, os
+from PIL import Image
+from skimage.metrics import structural_similarity
+import numpy as np
+
+# loading videointo open-cv
+path = os.path.join('web/uploads', 'demo0.mp4')
+capture=cv2.VideoCapture(path)
+
+
+def purge_duplicate(img0, img1, threshold, i=0):
+    sim, diff = structural_similarity(img0, img1, full=True)
+    if(sim < threshold):
+        return True
+    else:
+        return 
+
+i=0
+lag,frame=capture.read()
+img0=frame
+img0_bw=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+while(capture.isOpened()):
+    
+    img0=frame
+    img0_bw=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    flag,frame=capture.read()
+    if(flag==False):
+        break
+        
+    img1=frame
+    img1_bw=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    
+    if (purge_duplicate(img0_bw, img1_bw, 0.65, i) == True):
+        path='web/frame/'+str(i)+'.jpg'
+        cv2.imwrite(path,img0)
+        i+=1
+
+capture.release()
+cv2.destroyAllWindows()
